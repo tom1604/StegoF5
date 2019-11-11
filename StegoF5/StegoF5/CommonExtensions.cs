9056198eaf7fd94bdfcf65ade4364384dca1873b
tomaMin <tamara.minaeva@firstlinesoftware.com>
2019-11-11 18:00:52 +0300

    add main class, add methods for emded information add common class

---
+++ StegoF5/StegoF5/CommonExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace StegoF5
+{
+    internal static class CommonExtensions
+    {
+        internal static string GenerateBitsSequence(int bitsLength)
+        {
+            var random = new Random();
+            var resultString = new StringBuilder();
+
+            //генерация псевдослучайной битовой последовательности
+            for (var i = 0; i < bitsLength; i++)
+            {
+                resultString.Append(random.Next(0, 2));
+            }
+
+            return resultString.ToString();
+        }
+
+        internal static string ToBitsString(this string value)
+        {
+            return value.Select(x => Convert.ToString(x, 2).PadLeft(16, '0')).ToString();
+        }
+
+        internal static string ToBitsString(this Bitmap value)
+        {
+            var resultString = new StringBuilder();
+            for (var x = 0; x < value.Height; x++)
+            {
+                for (var y = 0; y < value.Width; y++)
+                {
+                    Color pixel = value.GetPixel(x, y);
+                    resultString.Append(Convert.ToString(pixel.R, 2).PadLeft(8, '0'));
+                    resultString.Append(Convert.ToString(pixel.G, 2).PadLeft(8, '0'));
+                    resultString.Append(Convert.ToString(pixel.B, 2).PadLeft(8, '0'));
+                }
+            }
+
+            return resultString.ToString();
+        }
+    }
+}
---
+++ StegoF5/StegoF5/StegoF5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegoF5
+{
+    public static class StegoF5
+    {
+        static StegoF5()
+        {
+
+        }
+
+        public static Bitmap EmbedInformation(this Bitmap image, int wordLength, int significantBitsLength,
+            Dictionary<string, bool[]>[] areaEmdedding, int bitsLength)
+        {
+            var bitsSequance = CommonExtensions.GenerateBitsSequence(bitsLength);
+            //встраивания сгенерированной битовой строки в контейнре
+            //Embed(areaEmdedding, sb.ToString());
+            return new Bitmap(20,20);
+        }
+
+        public static Bitmap EmbedInformation(this Bitmap image, int wordLength, int significantBitsLength,
+            Dictionary<string, bool[]>[] areaEmdedding, string text)
+        {
+            var bitsString = text.ToBitsString();
+            //встраивания битовой строки в контейнер
+            //Embed(areaEmdedding, sb.ToString());
+            return new Bitmap(20, 20);
+        }
+
+        public static Bitmap EmbedInformation(this Bitmap image, int wordLength, int significantBitsLength,
+            Dictionary<string, bool[]>[] areaEmdedding, Bitmap embeddableImage)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < bitmap.Height; x++)
+            {
+                for (int y = 0; y < bitmap.Width; y++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    sb.Append(Convert.ToString(pixel.R, 2).PadLeft(8, '0'));
+                    sb.Append(Convert.ToString(pixel.G, 2).PadLeft(8, '0'));
+                    sb.Append(Convert.ToString(pixel.B, 2).PadLeft(8, '0'));
+                }
+            }
+            //встраивание битовой строки в контейнер
+            //Embed(areaEmdedding, sb.ToString());
+            return new Bitmap(20, 20);
+        }
+    }
+}
