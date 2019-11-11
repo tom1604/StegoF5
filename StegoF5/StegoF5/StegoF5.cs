d20c5d886c519962f8e951604d5c63c35d0b3c79
tomaMin <tamara.minaeva@firstlinesoftware.com>
2019-11-11 18:01:50 +0300

    change with transfer image to string

--- StegoF5/StegoF5/StegoF5.cs
+++ StegoF5/StegoF5/StegoF5.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StegoF5
 {
@@ -35,17 +31,7 @@ namespace StegoF5
         public static Bitmap EmbedInformation(this Bitmap image, int wordLength, int significantBitsLength,
             Dictionary<string, bool[]>[] areaEmdedding, Bitmap embeddableImage)
         {
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < bitmap.Height; x++)
-            {
-                for (int y = 0; y < bitmap.Width; y++)
-                {
-                    Color pixel = bitmap.GetPixel(x, y);
-                    sb.Append(Convert.ToString(pixel.R, 2).PadLeft(8, '0'));
-                    sb.Append(Convert.ToString(pixel.G, 2).PadLeft(8, '0'));
-                    sb.Append(Convert.ToString(pixel.B, 2).PadLeft(8, '0'));
-                }
-            }
+            var bitsString = embeddableImage.ToBitsString();
             //встраивание битовой строки в контейнер
             //Embed(areaEmdedding, sb.ToString());
             return new Bitmap(20, 20);
